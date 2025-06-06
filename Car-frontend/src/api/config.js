7cc400206fdb994f5c97fc79ef6950408024b3ee
Aslbek0730 <aslbekakhmedov1@gmail.com>
2025-06-06 23:32:42 +0500

    tayyor

--- Car-frontend/src/api/config.js
+++ Car-frontend/src/api/config.js
@@ -15,10 +15,7 @@ const api = axios.create({
 // Request interceptor
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
+        // Token cookie orqali avtomatik yuboriladi
         return config;
     },
     (error) => {
@@ -31,7 +28,7 @@ api.interceptors.response.use(
     (response) => response,
     (error) => {
         if (error.response?.status === 401) {
-            localStorage.removeItem('token');
+            // Token muddati tugagan yoki noto'g'ri
             window.location.href = '/login';
         }
         return Promise.reject(error);
